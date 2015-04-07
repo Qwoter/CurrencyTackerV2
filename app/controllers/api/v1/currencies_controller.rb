aac56cbef89d28f4aa7a050d62b5d782ed880962
Nikita Kovalyov <laqwoter@gmail.com>
2015-04-07 03:03:51 +0300

    updated currency api with more actions

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class CurrenciesController < ApplicationController
+      before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+      before_action :setup_errors
+      # We don't use this cool rails feature to verify token parameter from client side form request so we disable it.
       skip_before_filter :verify_authenticity_token
 
       def index
@@ -10,6 +13,52 @@ module Api
       def show
         @currency = Currency.find(params[:id])
       end
+
+      def create
+        @currency = Currency.new(currency_params)
+
+        if @currency.save
+          render 'show', status: :created
+        else
+          @errors = @currency.errors.full_messages * "<br />"
+          render template: 'shared/error.json.jbuilder', status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @currency.update(currency_params)
+          head :no_content
+        else
+          @errors = @currency.errors.full_messages * "<br />"
+          render template: 'shared/error.json.jbuilder', status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @currency.destroy
+        head :no_content
+      end
+
+      private
+      # Setup errors.
+      def setup_errors
+        @errors = ""
+      end
+
+      # DRY.
+      def set_currency
+        @currency = Currency.find(params[:id])
+      end
+
+      # Set constrait on parameters that we get from internetz.
+      def currency_params
+        if params[:currency].blank?
+          {}
+        else
+          params.require(:currency).permit(:code, :name, :country_id, :weight, :collector_value)
+        end
+      end
     end
+
   end
 end
